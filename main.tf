6f0f1b15eae35e5da2576d1802281dc287613557
Nick Suckling <nicholas.suckling@hmcts.net>
2022-06-29 15:17:03 +0100

    fix(terraform): Fix terraform reference to tagging module

--- main.tf
+++ main.tf
@@ -4,7 +4,7 @@ resource "azurerm_resource_group" "rg1" {
 }
 
 module "tag_set" {
-  source         = "git::git@github.com:hmcts/cpp-terraform-tag-generator.git?ref=master"
+  source         = "github.com/hmcts/cpp-terraform-tag-generator"
   namespace      = var.namespace
   application    = var.application
   costcode       = var.costcode
