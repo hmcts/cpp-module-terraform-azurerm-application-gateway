c18d7d0ae1981ef5be0ed9953596c37dd1875a1a
varun.vijayagopal <varun.vijayagopal@hmcts.net>
2025-06-10 11:36:10 +0100

    module app gateway update

--- main.tf
+++ main.tf
@@ -233,7 +233,7 @@ resource "azurerm_application_gateway" "app_gateway" {
     for_each = var.trusted_root_certificates
     content {
       name = trusted_root_certificate.value.name
-      data = base64(trusted_root_certificate.value.data)
+      data = base64encode(trusted_root_certificate.value.data)
     }
   }
 
